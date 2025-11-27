3247fe6081b19b747c0b377dd11579fbd073323b
Gustavo Almeida Zuchini <gustavo.zuchini@etec.sp.gov.br>
2025-11-26 21:07:07 -0300

    Delete docs/js directory

--- docs/js/consulta-lista-psi.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const datas = document.querySelectorAll(".data");
-    datas.forEach(d => {
-        d.addEventListener("click", () => {
-            datas.forEach(dt => dt.classList.remove("ativo"));
-            d.classList.add("ativo");
-        });
-    });
-});
